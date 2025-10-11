53ef405191c21b6f8cfb40a506c97a08b8bd9c90
manya Shrivastava <manyashrivastava08@gmail.com>
2025-10-11 19:37:35 +0000

    why no css is used as i run in browser css is not appied to anything

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
